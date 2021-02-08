4ce969edacfa241c29ae61415fe48cc6a16e43f1
h4ckit <h4ckit@mail.ru>
2021-02-08 05:40:00 +0300

    board update

--- app/views.py
+++ app/views.py
@@ -14,21 +14,33 @@ def index():
         return render_template("index.html")
 
 
-@app.route('/board/<board_name>', methods=['GET', 'PUT'])
+@app.route('/board/<board_name>', methods=['GET'])
 def board(board_name):
+    if 'board_name' not in session:
+        return 'error session'
+    if session['board_name'] != board_name:
+        return 'error name'
+
     boards = Board.query.all()
-    board = None
+    _board = None
     for b in boards:
         if b.name == board_name:
-            board = b
+            _board = b
+
     if request.method == 'PUT':
-        if not board:
+        if not _board:
             return 'error'
-        return "test"
+        return "success"
     elif request.method == 'GET':
-        if not board:
-            board = Board(name=board_name)
+        if not _board:
+            _board = Board(name=board_name)
             db.session.add(board)
             db.session.commit()
         print(board.name)
-        return render_template("board.html")
+        return render_template("board.html", board_name=board_name)
+
+
+@app.roure('/board/<board_name>/update')
+def board_update(board_name):
+    if request.method == 'GET':
+        return {'id': 1, 'x': 100, 'y': 20}
