65efafb28cd736012570e02cbffe4d75c5748c54
LightVillet <maxim12000@ya.ru>
2021-02-08 05:19:48 +0300

    Add more javascript

--- app/static/js/board.js
+++ app/static/js/board.js
@@ -31,12 +31,31 @@ function drag_n_drop (elem)
 	}
 	elem.onmouseout = function(e)
 	{
-		console.log(elem.className, "out");
-		elem.onmousedown = null;
+		let relatedTarget = e.relatedTarget;
+		while (relatedTarget) {
+			if (relatedTarget == elem)
+			{
+				console.log(relatedTarget.className, "out");
+				elem.onmousedown = null;
+				return;
+			}
+		
+			relatedTarget = relatedTarget.parentNode;
+		  }
+
 	}
 }
 function createElem(e)
 {
+	let xmlHttp = new XMLHttpRequest();
+	xmlHttp.open("POST", '/board/asd', false);
+	let dic = {};
+	dic["x"] = e.pageX;
+	dic["y"] = e.pageY;
+	xmlHttp.send( dic );
+	let jsonData = JSON.parse(xmlHttp.responseText);
+	let id = jsonData['id'];
+	console.log( id );
 	const newDiv = document.createElement("div");
 	const newInput = document.createElement("input");
 	document.body.appendChild(newDiv);
@@ -44,6 +63,7 @@ function createElem(e)
 	newDiv.style.left = e.pageX;
 	newDiv.style.top = e.pageY;
 	newDiv.className = "divasd";
+	newDiv.id = id;
 	newInput.className = "inputField";
 	drag_n_drop(newDiv);
 	newDiv.append(newInput);
@@ -53,8 +73,11 @@ function createElem(e)
 	//newDiv.append(empty);
 
 }
+renderElem(elem)
+
 document.addEventListener('dblclick', function (e) {
-  createElem(e);
+	//let id = send_back(e);
+	createElem(e);
 });
 let button_test = document.body.getElementsByClassName("button_test")[0];
 let button_test_2 = document.body.getElementsByClassName("button_test")[1];
@@ -62,3 +85,19 @@ drag_n_drop(button_test);
 drag_n_drop(button_test_2);
 
 
+function send_back(e)
+{
+	
+	return;
+}
+
+function render()
+{
+	let xmlHttp = new XMLHttpRequest();
+	xmlHttp.open("GET", '/board/asd', false);
+	let jsonData = JSON.parse(xmlHttp.responseText);
+	for (let i in jsonData)
+	{
+		
+	}
+}
