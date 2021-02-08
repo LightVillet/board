0ce4b66047f4711cca11411a57ad726bf1d23957
h4ckit <h4ckit@mail.ru>
2021-02-08 03:55:53 +0300

    load Board, and fix migrations bd

--- app/models.py
+++ app/models.py
@@ -1,7 +1,9 @@
 from app import db
 
+
 class Board(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
 
     def __repr__(self):
-        return '<Board {}>'.format(self.id)
+        return '<Board {}>'.format(self.name)
--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
 from flask import render_template, request, redirect, url_for, session
 from app import app
+from .models import Board
+from app import db
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -12,6 +14,21 @@ def index():
         return render_template("index.html")
 
 
-@app.route('/board/<board_name>')
+@app.route('/board/<board_name>', methods=['GET', 'PUT'])
 def board(board_name):
-    return render_template("board.html")
+    boards = Board.query.all()
+    board = None
+    for b in boards:
+        if b.name == board_name:
+            board = b
+    if request.method == 'PUT':
+        if not board:
+            return 'error'
+        return "test"
+    elif request.method == 'GET':
+        if not board:
+            board = Board(name=board_name)
+            db.session.add(board)
+            db.session.commit()
+        print(board.name)
+        return render_template("board.html")
