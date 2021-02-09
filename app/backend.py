16cbb62735a6fc6ab8924654c984f1ea26b8b28f
h4ckit <h4ckit@mail.ru>
2021-02-09 23:49:30 +0300

    refactor socket

---
+++ app/backend.py
@@ -0,0 +1,26 @@
+from flask import session
+from models import Board, TextField
+from app import db
+
+
+def get_board():
+    boards_list = Board.query.all()
+    current_board = None
+    for b in boards_list:
+        if b.name == session['board_name']:
+            current_board = b
+    return current_board
+
+
+def create_text_field(x, y):
+    current_board = get_board()
+    text_fields_list = list(filter(lambda tf: tf.board_id == current_board.id, TextField.query.all()))
+    text_fields_ids = [t.id for t in text_fields_list]
+    if text_fields_ids:
+        new_id = max(text_fields_ids) + 1
+    else:
+        new_id = 1
+    new_text_field = TextField(id=new_id, x=x, y=y, board_id=current_board.id)
+    db.session.add(new_text_field)
+    db.session.commit()
+
--- app/sockets.py
+++ app/sockets.py
@@ -2,20 +2,16 @@ from flask import session
 from .models import Board, TextField
 from app import db, socketio
 from flask_socketio import emit, join_room
+from backend import get_board, create_text_field
 
 
 @socketio.event
 def connect():
     join_room(session['board_name'])
 
-    boards_list = Board.query.all()
-    current_board = None
-    for b in boards_list:
-        if b.name == session['board_name']:
-            current_board = b
+    current_board = get_board()
 
     text_fields_list = list(filter(lambda tf: tf.board_id == current_board.id, TextField.query.all()))
-
     data = []
     for tex_field in text_fields_list:
         data.append({
@@ -23,39 +19,45 @@ def connect():
             'x': tex_field.x,
             'y': tex_field.y
         })
-    emit('create', data, to=session['board_name'])
+
+    emit('init', data, to=session['board_name'])
 
 
 @socketio.event
-def update(message):
-    action = message['action']
+def create(message):
     data = message['data']
     x = data['x']
     y = data['y']
 
-    boards_list = Board.query.all()
-    current_board = None
-    for b in boards_list:
-        if b.name == session['board_name']:
-            current_board = b
-
-    if action == 'create':
-        text_fields_list = list(filter(lambda tf: tf.board_id == current_board.id, TextField.query.all()))
-        text_fields_ids = [t.id for t in text_fields_list]
-        if text_fields_ids:
-            new_id = max(text_fields_ids) + 1
-        else:
-            new_id = 1
-        new_text_field = TextField(id=new_id, x=x, y=y, board_id=current_board.id)
-        db.session.add(new_text_field)
-        db.session.commit()
-
-        message['data']['id'] = new_id
-    elif action == 'move':
-        text_field_id = data['id']
-        current_text_field = TextField.query.get(text_field_id)
-        current_text_field.x = x
-        current_text_field.y = y
-        db.session.commit()
+    new_id = create_text_field(x, y)
+    message['data']['id'] = new_id
 
     emit('update', message, to=session['board_name'])
+
+
+@socketio.event
+def move(message):
+    data = message['data']
+    x = data['x']
+    y = data['y']
+    text_field_id = data['id']
+
+    current_text_field = TextField.query.get(text_field_id)
+    current_text_field.x = x
+    current_text_field.y = y
+    db.session.commit()
+
+    emit('update', message, to=session['board_name'])
+
+
+@socketio.event
+def delete(message):
+    data = message['data']
+
+    text_field_id = data['id']
+    current_text_field = TextField.query.get(text_field_id)
+    db.session.delete(current_text_field)
+    db.session.commit()
+
+    emit('delete', message, to=session['board_name'])
+
