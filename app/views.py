7fe5bfce8fdeb281588d1865e6d8cf0146205d9d
h4ckit <h4ckit@mail.ru>
2021-02-09 16:46:56 +0300

    cleaning views

--- app/views.py
+++ app/views.py
@@ -21,7 +21,7 @@ def index():
         return render_template("index.html")
 
 
-@app.route('/board/<board_name>', methods=['GET'])
+@app.route('/board/<board_name>')
 def board(board_name):
     if 'board_name' not in session:
         return 'error session'
@@ -34,65 +34,9 @@ def board(board_name):
         if b.name == board_name:
             _board = b
 
-    if request.method == 'PUT':
-        if not _board:
-            return 'error'
-        return "success"
-    elif request.method == 'GET':
-        if not _board:
-            _board = Board(name=board_name)
-            db.session.add(_board)
-            db.session.commit()
-        return render_template("board.html", board_name=board_name)
-
-
-@app.route('/board/<board_name>/update', methods=['POST', 'GET', 'PUT'])
-def board_update(board_name):
-    if 'board_name' not in session:
-        return 'error session'
-    if session['board_name'] != board_name:
-        return 'error name'
-
-    boards = Board.query.all()
-    _board = None
-    for b in boards:
-        if b.name == board_name:
-            _board = b
     if not _board:
-        return 'error'
-
-    if request.method == 'GET':
-        text_fields = TextField.query.all()
-        _tf = list(filter(lambda tf: tf.board_id == _board.id, text_fields))
-        result = []
-        for t in _tf:
-            result.append({'id': t.id, 'x': t.x, 'y': t.y})
-        return jsonify(result)
-    elif request.method == 'POST':
-        data = json.loads(request.data)
-        x = data['x']
-        y = data['y']
-
-        list_t = list(filter(lambda tf: tf.board_id == _board.id, TextField.query.all()))
-        list_id = [t.id for t in list_t]
-        if list_id:
-            id_t = max(list_id) + 1
-        else:
-            id_t = 1
-        tf_ = TextField(id=id_t, x=x, y=y, board_id=_board.id)
-        db.session.add(tf_)
+        _board = Board(name=board_name)
+        db.session.add(_board)
         db.session.commit()
-        return jsonify({'id': tf_.id})
-    elif request.method == 'PUT':
-        data = json.loads(request.data)
-        x = data['x']
-        y = data['y']
-        id = data['id']
-        text_fields = TextField.query.all()
-        print(id, _board.id)
-        # print(text_fields)
-        tf = list(filter(lambda t: t.board_id == _board.id and t.id == id, text_fields))[0]
-        tf.x = x
-        tf.y = y
-        db.session.commit()
-        return 'test'
+
+    return render_template("board.html", board_name=board_name, async_mode=socketio.async_mode)
