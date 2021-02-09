0ad4b380f969526796b3e8a8f3fc02b9d25662fe
h4ckit <h4ckit@mail.ru>
2021-02-09 14:26:07 +0300

    __init__ settings

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,18 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_socketio import SocketIO
+from threading import Lock
 
 
 app = Flask(__name__)
-socketio = SocketIO(app)
 app.config.from_object('config')
+
+async_mode = None
+socketio = SocketIO(app, async_mode=async_mode)
+
+thread = None
+thread_lock = Lock()
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
