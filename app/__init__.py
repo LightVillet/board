823f0d89cb2170e34425e1310011e040488a8c03
h4ckit <h4ckit@mail.ru>
2021-02-09 18:24:13 +0300

    add sockets.py("update", "connect" functions), settings for __init__, change views imports

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_socketio import SocketIO
-from threading import Lock
 
 
 app = Flask(__name__)
@@ -11,11 +10,9 @@ app.config.from_object('config')
 async_mode = None
 socketio = SocketIO(app, async_mode=async_mode)
 
-thread = None
-thread_lock = Lock()
-
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 
 from app import views
+from app import sockets
