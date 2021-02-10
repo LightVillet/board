62fa6e751fa58dbc5ebcf699ebb60e8da0a9f46e
h4ckit <h4ckit@mail.ru>
2021-02-10 04:12:34 +0300

    back_feature flask reload without close

--- run.py
+++ run.py
@@ -2,4 +2,8 @@
 from app import socketio, app
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True, host='0.0.0.0', port='80')
+    while True:
+        try:
+            socketio.run(app, debug=True, host='0.0.0.0', port='80')
+        except Exception as e:
+            print(str(e))
